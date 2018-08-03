24b92d6f0eb0a265f9ae27d1a32004e1a175d138
Rik Wouters <rikwouters@sogyo.nl>
2018-08-03 16:43:40 +0200

    Now able to restore a backup.

--- EntoRestoreDatabase.sql
+++ EntoRestoreDatabase.sql
@@ -1,15 +1,13 @@
-USE EntoBase
+USE master
 GO
 
 ALTER DATABASE EntoBase 
 	SET SINGLE_USER 
 	WITH ROLLBACK IMMEDIATE  
-ALTER DATABASE EntoBase 
-	SET MULTI_USER
+
 
 RESTORE DATABASE EntoBase  
    FROM DISK = 'C:\Users\rikwouters\Documents\EntoBaseBackup\EntoBackup.bak'
 
-
-
---WERKT NIET
+ALTER DATABASE EntoBase 
+	SET MULTI_USER
